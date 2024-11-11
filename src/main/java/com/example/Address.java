80af05886192d4fdd26ebbd98dd4388cbb423ffe
Manish Dait <daitmanish88@gmail.com>
2024-11-11 22:21:00 +0530

    Implemented relationship in hibernate

---
+++ src/main/java/com/example/Address.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+  private String buildName;
+  private String street;
+  private String planet;
+
+  public Address() {}
+
+  public Address(String buildName, String street, String planet) {
+    this.buildName = buildName;
+    this.street = street;
+    this.planet = planet;
+  }
+
+  public String getBuildName() {
+    return buildName;
+  }
+
+  public void setBuildName(String buildName) {
+    this.buildName = buildName;
+  }
+
+  public String getStreet() {
+    return street;
+  }
+
+  public void setStreet(String street) {
+    this.street = street;
+  }
+
+  public String getPlanet() {
+    return planet;
+  }
+
+  public void setPlanet(String planet) {
+    this.planet = planet;
+  }
+}
--- src/main/java/com/example/Alien.java
+++ src/main/java/com/example/Alien.java
@@ -1,10 +1,18 @@
 package com.example;
 
+import java.util.List;
+
+// import jakarta.persistence.AttributeOverride;
+// import jakarta.persistence.AttributeOverrides;
+// import jakarta.persistence.Embedded;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+// import jakarta.persistence.OneToMany;
+// import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "alien_table")
@@ -15,14 +23,24 @@ public class Alien {
   @Column(name = "alien_name")
   private String name;
 
-  @Transient
-  private String secret;
+  // @Embedded
+  // @AttributeOverrides({
+  //   @AttributeOverride(name = "buildName", column = @Column(name = "building"))
+  // })
+  // private Address address;
+
+  // @OneToMany(mappedBy = "alien")
+  // private List<Laptop> laptop;
+
+  @ManyToMany(mappedBy = "alien")
+  private List<Laptop> laptop;
 
   public Alien() {}
 
-  public Alien(int id, String name) {
+  public Alien(int id, String name, List<Laptop> laptop) {
     this.id = id;
     this.name = name;
+    this.laptop = laptop;
   }
 
   public int getId() {
@@ -41,8 +59,33 @@ public class Alien {
     this.name = name;
   }
 
+  // public Address getAddress() {
+  //   return address;
+  // }
+
+  // public void setAddress(Address address) {
+  //   this.address = address;
+  // }
+
+  // public Laptop getLaptop() {
+  //   return laptop;
+  // }
+
+  // public void setLaptop(Laptop laptop) {
+  //   this.laptop = laptop;
+  // }
+
+  public List<Laptop> getLaptop() {
+    return laptop;
+  }
+
+  public void setLaptop(List<Laptop> laptop) {
+    this.laptop = laptop;
+  }
+  
   @Override
   public String toString() {
-    return "Alien [id=" + id + ", name=" + name + ", secret=" + secret + "]";
+    return "Alien [id=" + id + ", name=" + name + ", laptop=" + laptop + "]";
   }
+
 }
